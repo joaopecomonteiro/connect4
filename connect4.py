50860afe4d5cd959879fdc2e20420596b060e523
João Monteiro <joaomonteiro1111@gmail.com>
2023-04-18 20:02:13 +0100

    Update connect4.py

--- connect4.py
+++ connect4.py
@@ -45,7 +45,6 @@ class MCTS():
     def mcts_drop_piece(self, board, row, col, piece):
         temp_board = board.copy()
         temp_board[row][col] = piece
-        # print(np.flip(temp_board , 0))
         return temp_board
 
 
@@ -75,7 +74,6 @@ class MCTS():
             col = self.rollout_policy(possible_moves)
             row = open_row(current_rollout_state, col)
             current_rollout_state = self.mcts_drop_piece(current_rollout_state, row, col, cur_piece)
-            # print(np.flip(current_rollout_state,0))
             if cur_piece == 1:
                 cur_piece = 2
             else:
@@ -165,25 +163,21 @@ def print_board(board):
 
 
 def winning_move(board, piece):
-    # Check horizontal locations for win
     for c in range(column_count-3):
         for r in range(row_count):
             if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
                 return True
  
-    # Check vertical locations for win
     for c in range(column_count):
         for r in range(row_count-3):
             if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
                 return True
  
-    # Check positively sloped diaganols
     for c in range(column_count-3):
         for r in range(row_count-3):
             if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
                 return True
  
-    # Check negatively sloped diaganols
     for c in range(column_count-3):
         for r in range(3, row_count):
             if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
@@ -226,11 +220,9 @@ def score_position(board, player):
 
     score = 0
 
-    # Score horizontal positions
     for r in range(row_count):
         row_array = [int(i) for i in list(board[r,:])]
         for c in range(column_count - 3):
-            # Create a horizontal window of 4
             window = row_array[c:c + 4]
             if window.count(player) == 4:
                 return 512
@@ -238,11 +230,9 @@ def score_position(board, player):
                 return -512
             score += evaluate_window(window, player)
 
-    # Score vertical positions
     for c in range(column_count):
         col_array = [int(i) for i in list(board[:, c])]
         for r in range(row_count - 3):
-            # Create a vertical window of 4
             window = col_array[r:r + 4]
             if window.count(player) == 4:
                 return 512
@@ -250,10 +240,8 @@ def score_position(board, player):
                 return -512
             score += evaluate_window(window, player)
 
-    # Score positive diagonals
     for r in range(row_count - 3):
         for c in range(column_count - 3):
-            # Create a positive diagonal window of 4
             window = [board[r + i][c + i] for i in range(4)]
             if window.count(player) == 4:
                 return 512
@@ -261,10 +249,8 @@ def score_position(board, player):
                 return -512
             score += evaluate_window(window, player)
 
-    # Score negative diagonals
     for r in range(row_count - 3):
         for c in range(column_count - 3):
-            # Create a negative diagonal window of 4
             window = [board[r + 3 - i][c + i] for i in range(4)]
             if window.count(player) == 4:
                 return 512
@@ -297,9 +283,9 @@ def minimax(board, depth, maximizingPlayer):
                     return (None, 100000000000000)
                 elif winning_move(board, player1_piece):
                     return (None, -10000000000000)
-                else: # Game is over, no more valid moves
+                else: 
                     return (None, 0)
-            else: # Depth is zero
+            else: 
                 return (None, score_position(board, player2_piece))
         if maximizingPlayer:
             value = -math.inf
@@ -314,13 +300,51 @@ def minimax(board, depth, maximizingPlayer):
                     column = col
             return column, value
 
-        else: # Minimizing player
+        else: 
             value = math.inf
             column = random.choice(valid_locations)
             for col in valid_locations:
                 row = open_row(board, col)
                 b_copy = board.copy()
                 drop_piece(b_copy, row, col, player1_piece)
+                new_score = minimax(b_copy, depth-1, True)[1]
+                if new_score < value:
+                    value = new_score
+                    column = col
+            return column, value
+    else:
+        valid_locations = get_valid_locations(board)
+        is_terminal = is_terminal_node(board)
+        if depth == 0 or is_terminal:
+            if is_terminal:
+                if winning_move(board, player1_piece):
+                    return (None, 100000000000000)
+                elif winning_move(board, player2_piece):
+                    return (None, -10000000000000)
+                else: 
+                    return (None, 0)
+            else: 
+                return (None, score_position(board, player1_piece))
+        if maximizingPlayer:
+            value = -math.inf
+            column = random.choice(valid_locations)
+            for col in valid_locations:
+                row = open_row(board, col)
+                b_copy = board.copy()
+                drop_piece(b_copy, row, col, player1_piece)
+                new_score = minimax(b_copy, depth-1, False)[1]
+                if new_score > value:
+                    value = new_score
+                    column = col
+            return column, value
+
+        else:
+            value = math.inf
+            column = random.choice(valid_locations)
+            for col in valid_locations:
+                row = open_row(board, col)
+                b_copy = board.copy()
+                drop_piece(b_copy, row, col, player2_piece)
                 new_score = minimax(b_copy, depth-1, True)[1]
                 if new_score < value:
                     value = new_score
@@ -334,21 +358,21 @@ def alpha_beta(board, depth, alpha, beta, maximizingPlayer):
         is_terminal = is_terminal_node(board)
         if depth == 0 or is_terminal:
             if is_terminal:
-                if winning_move(board, 2):
+                if winning_move(board, player2_piece):
                     return (None, 100000000000000)
-                elif winning_move(board, 1):
+                elif winning_move(board, player1_piece):
                     return (None, -10000000000000)
-                else: # Game is over, no more valid moves
+                else:
                     return (None, 0)
-            else: # Depth is zero
-                return (None, score_position(board, 2))
+            else:
+                return (None, score_position(board, player2_piece))
         if maximizingPlayer:
             value = -math.inf
             column = random.choice(valid_locations)
             for col in valid_locations:
                 row = open_row(board, col)
                 b_copy = board.copy()
-                drop_piece(b_copy, row, col, 2)
+                drop_piece(b_copy, row, col, player2_piece)
                 new_score = alpha_beta(b_copy, depth-1, alpha, beta, False)[1]
                 if new_score > value:
                     value = new_score
@@ -358,13 +382,57 @@ def alpha_beta(board, depth, alpha, beta, maximizingPlayer):
                     break
             return column, value
 
-        else: # Minimizing player
+        else: 
             value = math.inf
             column = random.choice(valid_locations)
             for col in valid_locations:
                 row = open_row(board, col)
                 b_copy = board.copy()
-                drop_piece(b_copy, row, col, 1)
+                drop_piece(b_copy, row, col, player1_piece)
+                new_score = alpha_beta(b_copy, depth-1, alpha, beta, True)[1]
+                if new_score < value:
+                    value = new_score
+                    column = col
+                beta = min(beta, value)
+                if alpha >= beta:
+                    break
+            return column, value
+    else:
+        valid_locations = get_valid_locations(board)
+        is_terminal = is_terminal_node(board)
+        if depth == 0 or is_terminal:
+            if is_terminal:
+                if winning_move(board, player1_piece):
+                    return (None, 100000000000000)
+                elif winning_move(board, player2_piece):
+                    return (None, -10000000000000)
+                else:
+                    return (None, 0)
+            else:
+                return (None, score_position(board, player1_piece))
+        if maximizingPlayer:
+            value = -math.inf
+            column = random.choice(valid_locations)
+            for col in valid_locations:
+                row = open_row(board, col)
+                b_copy = board.copy()
+                drop_piece(b_copy, row, col, player1_piece)
+                new_score = alpha_beta(b_copy, depth-1, alpha, beta, False)[1]
+                if new_score > value:
+                    value = new_score
+                    column = col
+                alpha = max(alpha, value)
+                if alpha >= beta:
+                    break
+            return column, value
+
+        else: 
+            value = math.inf
+            column = random.choice(valid_locations)
+            for col in valid_locations:
+                row = open_row(board, col)
+                b_copy = board.copy()
+                drop_piece(b_copy, row, col, player2_piece)
                 new_score = alpha_beta(b_copy, depth-1, alpha, beta, True)[1]
                 if new_score < value:
                     value = new_score
@@ -384,7 +452,6 @@ def greedy(board, piece):
         temp_board = board.copy()
         drop_piece(temp_board, row, col, piece)
         score = score_position(temp_board, piece)
-        print(f"Score: {score}, col: {col+1}")
         if score > best_score:
             best_score = score
             best_col = col
@@ -433,7 +500,6 @@ myfont = pygame.font.SysFont("monospace", 50)
 player1_piece = 1
 player2_piece = 2
 
-
 while not game_over:
     if turn == 0:
         if player_1 == 1:
@@ -466,7 +532,6 @@ while not game_over:
                             label = myfont.render(f"Player {player1_piece} wins!!", 1, RED)
                             screen.blit(label, (40,10))
                             game_over = True
-                        print(score_position(board, player1_piece))
                         print_board(board)
                         draw_board(board)
                         turn += 1
@@ -494,11 +559,52 @@ while not game_over:
                         label = myfont.render(f"Player {player1_piece} wins!!", 1, RED)
                         screen.blit(label, (40,10))
                         game_over = True
-                    print(score_position(board, player1_piece))
                     print_board(board)
                     draw_board(board)
                     turn += 1
                     turn = turn % 2
+        
+        if player_1 == 3:
+            time.sleep(1)
+            col = minimax(board, 5, True)[0]
+            if is_valid_location(board, col):
+                row = open_row(board, col)
+                drop_piece(board, row, col, player1_piece)
+                if len(get_valid_locations(board)) == 0:
+                    print(f"Empate!!")
+                    label = myfont.render(f"Empate!!", 1, BLUE)
+                    screen.blit(label, (40,10))
+                    game_over = True
+                if winning_move(board, player1_piece):
+                    print(f"Jogador {player1_piece} ganhou!!")
+                    label = myfont.render(f"Player {player1_piece} wins!!", 1, RED)
+                    screen.blit(label, (40,10))
+                    game_over = True
+                print_board(board)
+                draw_board(board)
+                turn += 1
+                turn = turn % 2
+
+        elif player_1 == 4:
+            time.sleep(1)
+            col = alpha_beta(board, 5, -math.inf, math.inf, True)[0]
+            if is_valid_location(board, col):
+                row = open_row(board, col)
+                drop_piece(board, row, col, player1_piece)
+                if len(get_valid_locations(board)) == 0:
+                    print(f"Empate!!")
+                    label = myfont.render(f"Empate!!", 1, BLUE)
+                    screen.blit(label, (40,10))
+                    game_over = True
+                if winning_move(board, player1_piece):
+                    print(f"Jogador {player1_piece} ganhou!!")
+                    label = myfont.render(f"Player {player1_piece} wins!!", 1, RED)
+                    screen.blit(label, (40,10))
+                    game_over = True
+                print_board(board)
+                draw_board(board)
+                turn += 1
+                turn = turn % 2
 
     else:
         if player_2 == 1:
@@ -531,7 +637,6 @@ while not game_over:
                             label = myfont.render(f"Player {player2_piece} wins!!", 1, RED)
                             screen.blit(label, (40,10))
                             game_over = True
-                        print(score_position(board, player2_piece))
                         print_board(board)
                         draw_board(board)
                         turn += 1
@@ -545,7 +650,6 @@ while not game_over:
             if is_valid_location(board, col):
                 row = open_row(board, col)
                 drop_piece(board, row, col, player2_piece)
-                print(score_position(board, player2_piece))
                 if len(get_valid_locations(board)) == 0:
                     print(f"Empate!!")
                     label = myfont.render(f"Empate!!", 1, BLUE)
@@ -556,7 +660,6 @@ while not game_over:
                     label = myfont.render(f"Player {player2_piece} wins!!", 1, RED)
                     screen.blit(label, (40,10))
                     game_over = True
-                print(score_position(board, player2_piece))
                 print_board(board)
                 draw_board(board)
                 turn += 1
@@ -565,11 +668,10 @@ while not game_over:
 
         elif player_2 == 3:
             time.sleep(1)
-            col = minimax(board, 5, -math.inf, math.inf, True)[0]
+            col = minimax(board, 5, True)[0]
             if is_valid_location(board, col):
                 row = open_row(board, col)
                 drop_piece(board, row, col, player2_piece)
-                print(score_position(board, player2_piece))
                 if len(get_valid_locations(board)) == 0:
                     print(f"Empate!!")
                     label = myfont.render(f"Empate!!", 1, BLUE)
@@ -577,12 +679,11 @@ while not game_over:
                     game_over = True
                 if winning_move(board, player2_piece):
                     print(f"Jogador {player2_piece} ganhou!!")
-                    label = myfont.render(f"Player {player2_piece} wins!!", 1, RED)
+                    label = myfont.render(f"Player {player2_piece} wins!!", 1, YELLOW)
                     screen.blit(label, (40,10))
                     game_over = True
                 print_board(board)
                 draw_board(board)
-                print(score_position(board, player2_piece))
                 turn += 1
                 turn = turn % 2
 
@@ -594,7 +695,6 @@ while not game_over:
             if is_valid_location(board, col):
                 row = open_row(board, col)
                 drop_piece(board, row, col, player2_piece)
-                print(score_position(board, player2_piece))
                 if len(get_valid_locations(board)) == 0:
                     print(f"Empate!!")
                     label = myfont.render(f"Empate!!", 1, BLUE)
@@ -602,12 +702,11 @@ while not game_over:
                     game_over = True
                 if winning_move(board, player2_piece):
                     print(f"Jogador {player2_piece} ganhou!!")
-                    label = myfont.render(f"Player {player2_piece} wins!!", 1, RED)
+                    label = myfont.render(f"Player {player2_piece} wins!!", 1, YELLOW)
                     screen.blit(label, (40,10))
                     game_over = True
                 print_board(board)
                 draw_board(board)
-                print(score_position(board, player2_piece))
                 turn += 1
                 turn = turn % 2
 
@@ -627,7 +726,6 @@ while not game_over:
                 game_over = True
             print_board(board)
             draw_board(board)
-            print(score_position(board, player2_piece))
             turn += 1
             turn = turn % 2
 
